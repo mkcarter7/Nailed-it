9bc55660f2d1dfd4c1ef69d514482544a3732616
Melissa Carter <mkd.princess@gmail.com>
2025-02-25 20:26:45 -0600

    set up files

--- nailedit/urls.py
+++ nailedit/urls.py
@@ -1,4 +1,4 @@
-"""tunapiano URL Configuration
+"""nailedit URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
